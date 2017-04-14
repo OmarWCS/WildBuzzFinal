026b091da400d522d2b47e5417f41a8526de57e4
omarWCS <omar.mbengue31000@gmail.com>
2017-04-14 17:32:21 +0200

    master

---
+++ app/src/main/java/com/example/apprenti/wildbuzz/Rank.java
@@ -0,0 +1,13 @@
+package com.example.apprenti.wildbuzz;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class Rank extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rank);
+    }
+}
