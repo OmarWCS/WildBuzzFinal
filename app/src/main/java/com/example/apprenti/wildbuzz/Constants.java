655177f907b5a1bf2b2d4005cac01034562bc308
apprenti <bravo.pauline@gmail.com>
2017-04-19 12:04:32 +0200

    contributions A  apparaissent dans firebase

---
+++ app/src/main/java/com/example/apprenti/wildbuzz/Constants.java
@@ -0,0 +1,12 @@
+package com.example.apprenti.wildbuzz;
+
+/**
+ * Created by apprenti on 18/04/17.
+ */
+
+public class Constants {
+
+    public static final String DATABASE_PATH_CONTRIBUTIONS = "contributions";
+    public static final String DATABASE_PATH_UPLOADS = "uploads";
+    public static final String DATABASE_PATH_ALL_UPLOADS ="allContributions";
+}
